8f3badc750d3cd406f1d3efab00c7874fe82da39
YourGitHubUsername <YourGitHubEmail>
2024-01-12 21:01:22 +0100

    start

---
+++ ex01/phonebook.cpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   phonebook.cpp                                      :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/01/12 21:01:05 by dreijans      #+#    #+#                 */
+/*   Updated: 2024/01/12 21:01:11 by dreijans      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream> //using namespace std
+
+class PhoneBook //Class
+{       
+  	public:           // Access specifier
+    int myNum;        // Attribute (int variable)
+    string myString;  // Attribute (string variable)
+};
