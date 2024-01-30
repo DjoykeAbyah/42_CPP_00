73654b459004c608729d0b2def57bfb4d48afd09
YourGitHubUsername <YourGitHubEmail>
2024-01-30 21:02:32 +0100

    need to make constructor

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -6,11 +6,12 @@
 /*   By: dreijans <dreijans@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/01/29 18:25:35 by dreijans      #+#    #+#                 */
-/*   Updated: 2024/01/30 19:22:26 by dreijans      ########   odam.nl         */
+/*   Updated: 2024/01/30 21:01:26 by dreijans      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Account.hpp"
+#include <iostream>
 
 Account::Account(int initial_deposit){};
 
@@ -30,6 +31,7 @@ int Account::getTotalAmount()
 
 int Account::getNbDeposits()
 {
+	//how to create total number of deposits?
 	return _totalNbDeposits;
 }
 
@@ -38,23 +40,39 @@ int Account::getNbWithdrawals()
 	return _totalNbWithdrawals;
 }
 
+/**
+ * @todo add timestamp
+ * [19920104_091532] accounts:8;total:20049;deposits:0;withdrawals:0
+*/
 void Account::displayAccountsInfos()
 {
-	
+    std::cout << " accounts:" << getNbAccounts() << ";"
+              << "total:" << getTotalAmount() << ";"
+              << "deposits:" << getNbDeposits() << ";"
+              << "withdrawals:" << getNbWithdrawals() << std::endl;
 }
 
+/**
+ * make disposit so
+ * _amount
+ * _nbDeposits
+ * 
+*/
 void Account::makeDeposit(int deposit)
 {
-	_nbDeposits += 1;//or
-	_totalNbDeposits += 1;
+	deposit += _amount;
+	_nbDeposits += 1;
 	deposit += _totalAmount;
+	_totalNbDeposits += 1;
 }
 
 bool Account::makeWithdrawal(int withdrawal)
 {
-	_nbWithdrawals += 1;//or
-	_totalNbWithdrawals += 1;
+	withdrawal -= _amount;
+	_nbWithdrawals += 1;
 	withdrawal -= _totalAmount;
+	_totalNbWithdrawals += 1;
+	return true;
 }
 
 int Account::checkAmount()const
@@ -62,6 +80,10 @@ int Account::checkAmount()const
 	return _totalAmount;
 }
 
+/**
+ * @todo add timestamp
+ * [19920104_091532] index:0;amount:42;deposits:0;withdrawals:0
+*/
 void Account::displayStatus()const
 {
 	
